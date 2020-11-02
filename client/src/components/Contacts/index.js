dca5f1e963992c6e7007c70f92007c0f2c02edab
Алена <alena.lubanko@yandex.ru>
2020-11-02 15:46:58 +0300

    update component contacts

--- client/src/components/Contacts/index.js
+++ client/src/components/Contacts/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import MailIcon from '@material-ui/icons/Mail';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
+import FaceIcon from '@material-ui/icons/Face';
 
 function Contacts() {
   return (
-    <div>LUBANKO</div>
+    <>
+      <table>
+        <tr>
+          <td>
+            <label><FaceIcon /></label>
+          </td>
+          <td>Приложение разработано: Alena Lubanko</td>
+        </tr>
+        <tr>
+          <td>
+            <label><PhoneAndroidIcon /></label>
+          </td>
+          <td>8-953-512-93-74</td>
+        </tr>
+        <tr>
+          <td>
+            <label><MailIcon /></label>
+          </td>
+          <td>alena.lubanko@yandex.ru</td>
+        </tr>
+      </table>
+    </>
   )
 }
 
